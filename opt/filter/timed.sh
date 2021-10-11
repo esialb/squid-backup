ecc8b5f61e814bc064b06defd6d3520bddbffed9
Robin Kirkman <robin@robinkirkman.com>
2021-10-11 08:46:08 -0400

    Mon 11 Oct 2021 08:46:08 AM EDT

--- opt/filter/timed.sh
+++ opt/filter/timed.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd "$(dirname "$(readlink -f "$0")")"
-
 TIME="$1"
 shift
+DEVICES="$(cat "$@")"
 
-for DEVICE in "$@"; do
-	TARGET="/opt/filter/bypass/timed/$(date '+%F-%H-%M-%S')_$TIME_$(basename "$DEVICE")"
-	cp "$DEVICE" "$TARGET"
-	at -M "$TIME" <<FFF
+cd "$(dirname "$(readlink -f "$0")")"
+
+TARGET="/opt/filter/bypass/timed/$(date '+%F-%H-%M-%S')_$TIME"
+echo "$DEVICES" > "$TARGET"
+at -M "$TIME" <<FFF
 	rm "$TARGET"
 	/opt/filter/apply.sh
 FFF
-done
+
 ./apply.sh
 
