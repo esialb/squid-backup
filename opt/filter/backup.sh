82301754b0064af448a438110cc87417d15a773b
Robin Kirkman <robin@robinkirkman.com>
2021-10-11 07:39:47 -0400

    Mon 11 Oct 2021 07:39:47 AM EDT

---
+++ opt/filter/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for FILE in \
+	/opt/filter/*.sh \
+	/etc/squid/*.conf /etc/squid/*.txt \
+	/etc/network/interfaces \
+	/var/lib/squidguard/db/kids/domains \
+	; do
+	mkdir -p "squid-backup/$(dirname $FILE)"
+	cp "$FILE" "squid-backup/$FILE"
+done
+
+cd squid-backup
+git add .
+git commit -m "$(date)"
+git push
+
