64c9f6675740d8d55bd5a8522e09a1bf5c48f2ed
Robin Kirkman <robin@robinkirkman.com>
2021-10-11 07:31:46 -0400

    Mon 11 Oct 2021 07:31:46 AM EDT

--- root/ebtables_exclusions.sh
+++ root/ebtables_exclusions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for MAC in `cat mac_exclusions.txt`; do
+cd "$(dirname "$(readlink -f "$0")")"
+
+for MAC in `cat filter-disabled/*`; do
 	ebtables-legacy -t broute -I BROUTING 1 -s $MAC -j ACCEPT
 	ebtables-legacy -t broute -I BROUTING 1 -d $MAC -j ACCEPT
 done
