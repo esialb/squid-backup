2c23bebc80c2949439b5f3bdd736556c402feca7
Robin Kirkman <robin@robinkirkman.com>
2021-10-11 13:24:43 -0400

    Mon 11 Oct 2021 01:24:43 PM EDT

--- filter/status.sh
+++ filter/status.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 BYPASSED="$(find bypass/ -not -type d -exec cat {} \; | sort | uniq | grep :)"
+BLOCKED="$(find block/ -not -type d -exec cat {} \; | sort | uniq | grep :)"
+
 for MAC in $BYPASSED; do
-	grep -l -F $MAC `find devices/ -not -type d`
+	grep -l -F $MAC `find devices/ -not -type d` | sed -r 's/^/BYPASS FILTERS:   /g'
 done
 
+for MAC in $BYPASSED; do
+BLOCKED="$(echo $BLOCKED | sed -r "s/$MAC//g")"
+done
+
+for MAC in $BLOCKED; do
+	grep -l -F $MAC `find devices/ -not -type d` | sed -r 's/^/BLOCK EVERYTHING: /g'
+done
